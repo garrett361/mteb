7cd87979e0a081cbb40e72b6ef29bf966363e198
Garrett Goon <garrett.goon@hpe.com>
2023-09-23 11:58:44 -0400

    add omegaconf

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
         "evaluate==0.4.0",
         "InstructorEmbedding==1.0.1",
         "pytorch-extension==0.2",
+        "omegaconf==2.3.0",
     ],
     extras_require=extras,
     classifiers=[
